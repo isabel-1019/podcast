29fd4e21a0d42936d1a4ed4f7023c1de84459790
isabel-1019 <baigen.jarillo.maria.isabel.m352@gmail.com>
2026-05-27 11:27:01 -0600

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Conexión a MongoDB (Pon aquí tu enlace real de Atlas)
-const MONGO_URI = process.env.MONGO_URI || 'tu_conexion_real_de_mongodb_atlas';
+const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://moralesolguinivanmauriciom352_db_user:Jr2tJL3phVZp8k@podcast.zpapegt.mongodb.net/?appName=podcast';
 mongoose.connect(MONGO_URI)
   .then(() => console.log('Conectado exitosamente a MongoDB Atlas'))
   .catch(err => console.error('Error al conectar a MongoDB:', err));
@@ -36,7 +36,7 @@ app.post('/usuarios', async (req, res) => {
 // Cuando la gente entre a tu link de Render, se les abrirá tu diseño al instante sin pantallas en blanco
 app.get('*', (req, res) => {
   // Pega aquí adentro el enlace que te dio FlutterFlow en el Paso 1
-  res.redirect('https://TU-APP-DE-FLUTTERFLOW.flutterflow.app');
+  res.redirect('https://mindstream-podcast-app-template-z4dhrq.flutterflow.app');
 });
 
 const PORT = process.env.PORT || 3000;
